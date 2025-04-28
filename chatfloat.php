19fbcc6c699030f97933f45b64ee490cf720fc71
Safeer E Hussain <safeer.husssain@gmail.com>
2025-04-28 09:27:39 +0500

    Update chatfloat.php

--- chatfloat.php
+++ chatfloat.php
@@ -304,7 +304,7 @@ function chatfloat_display_field_callback() {
 
 
 // Dark mode option
-function chatfloat_darkmode_callback() {
+function chatfloat_darkmode_field_callback() {
     $dark_mode = get_option('chatfloat_darkmode', 'no'); // Default to no
     ?>
 
